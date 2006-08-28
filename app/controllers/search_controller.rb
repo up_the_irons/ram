0c01d49cd05dec49bf268635facbcc4da019cfab
Mark Daggett <heavysixer@gmail.com>
2006-08-28 07:41:48 +0000

    fixed the search results some.
    git-svn-id: svn+ssh://svn.locusfoc.us/home/svn/ram/trunk@200 7bd17f4f-1217-0410-bbc5-bc4367116c30

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,5 @@
 class SearchController < ProtectedController
   def all
     @assets = Asset.search(params[:query], current_user.groups.map { |o| o.id })
-    render :partial=>'assets/list'
   end
 end
