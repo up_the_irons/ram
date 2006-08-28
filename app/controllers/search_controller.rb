3580b6328b27389d97e9bf9f63deaab2dda2a63b
Mark Daggett <heavysixer@gmail.com>
2006-08-28 07:33:09 +0000

    made a small tweak.
    git-svn-id: svn+ssh://svn.locusfoc.us/home/svn/ram/trunk@197 7bd17f4f-1217-0410-bbc5-bc4367116c30

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ProtectedController
   def all
     @assets = Asset.search(params[:query], current_user.groups.map { |o| o.id })
+    render :partial=>'assets/list'
   end
 end
