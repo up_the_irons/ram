39f6ea2b78803fa9d800118bce06d32adf81de83
Mark Daggett <heavysixer@gmail.com>
2006-10-16 05:09:50 +0000

    opps. deleted the wrong bit.
    git-svn-id: svn+ssh://svn.locusfoc.us/home/svn/ram/trunk@662 7bd17f4f-1217-0410-bbc5-bc4367116c30

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -65,11 +65,11 @@ module ApplicationHelper
        else
          name = "#{opts[:model]}[#{opts[:many_object]}][]"
        end
-        onclick_method = "#{opts[:unselect_method]}(#{b[:id]});}"
+        onclick_method = "#{opts[:unselect_method]}(#{b[:id]});"
        
        link = %{<input type='checkbox' style="display:none;" name="#{name}" value="#{b[:id]}" id="branch_checkbox_#{b[:id]}" />} 
        link << image_tag('icons/tick.png',{:style=>"display:none",:id=>"tick_#{b[:id]}"})
-       link << link_to(truncate(b[:name],25),"#checkbox_#{b[:id]}",{:onclick=>onclick_method)
+       link << link_to(truncate(b[:name],25),"#checkbox_#{b[:id]}",{:onclick=>onclick_method})
        if b[:children].size > 0
          code << "<li id=\"selectable_branch_#{b[:id]}\">#{link}\n\r" 
          code << "<ul>#{parse_selectable_tree(b[:children],opts)}</ul></li>\n\r"
