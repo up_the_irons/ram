c116f58a14090d99582abc7795813d31a80939ce
Mark Daggett <heavysixer@gmail.com>
2006-10-23 21:30:02 +0000

    fixed the bug in initializable. This is not what ticket 95 was for but i wanted to add it so I am going to make a new branch.
    git-svn-id: svn+ssh://svn.locusfoc.us/home/svn/ram/trunk@717 7bd17f4f-1217-0410-bbc5-bc4367116c30

--- app/controllers/feed_reader.rb
+++ app/controllers/feed_reader.rb
@@ -1,26 +1,26 @@
-#basic RSS aggregator mixin.
-#takes a valid RSS feed (not Atom feed) and parses each item of the feed into an instance of the RSS::Rss::Channel::Item class.
-#the class has these instance_varialbes [@guid, @author, @title, @converter, @category, @description, @link, @enclosure, @source, @do_validate, @pubDate, @comments]
+# Basic RSS aggregator mixin.
+# Takes a valid RSS feed (not Atom feed) and parses each item of the feed into an instance of the RSS::Rss::Channel::Item class.
+# The class has these instance_varialbes [@guid, @author, @title, @converter, @category, @description, @link, @enclosure, @source, @do_validate, @pubDate, @comments]
 
 require 'rss/2.0'
 require 'open-uri'
 require 'initializable'
 module FeedReader
-  include Initializable #when including this module DONOT call the self.included callback because it will override Initializable
+  include Initializable # When including this module DO NOT call the self.included callback because it will override Initializable
   
   def self.initialize(class_instance)
 
-    #inject the needed instance variables for the feedreader. The initializable module takes care of the auto loading of this module within the mixee class
-    #however, you still need to do a instance_eval to set the needed variables for the feed_reader functionality.
+    # Inject the needed instance variables for the feedreader. The initializable module takes care of the auto loading of this module within the mixee class
+    # however, you still need to do a instance_eval to set the needed variables for the feed_reader functionality.
     [{:key=>'feeds',:value=>[]}, {:key=>'feed_urls',:value=>[]}].each do |h|
       
-      #generic accessor for instance_variables
+      # Generic accessor for instance_variables
       attr_accessor "#{h[:key].to_sym}"
 
-      #only create these instance variabes if they don't already exisit in the mixee class.
+      # Only create these instance variabes if they don't already exisit in the mixee class.
       class_instance.instance_eval do
-        instance_var = "@#{h[:key]}"
-        class_instance.instance_variable_set(instance_var.to_sym, h[:value] ) unless class_instance.instance_variables.include?(instance_var)
+         instance_var = "@#{h[:key]}"
+         class_instance.instance_variable_set(instance_var.to_sym, h[:value] ) unless class_instance.instance_variables.include?(instance_var)
       end
     end
     
--- app/controllers/initializable.rb
+++ app/controllers/initializable.rb
@@ -1,18 +1,18 @@
-#modify the Ruby the 'Class' class to affored automatic initialization of mixed-in modules.
+# Modify the Ruby the 'Class' class to affored automatic initialization of mixed-in modules.
 class Class
-  def included_modules
-    @included_modules ||= []
-  end
-  
-  alias_method :old_new, :new
-  def new(*args, &block)
-    obj = old_new(*args, &block)
-    self.included_modules.each do |mod|
-      mod.initialize(obj) if mod.respond_to?(:initialize)
-    end
-    obj
-  end
-end
+   def initialized_modules
+     @initialized_modules ||= []
+   end
+   
+   alias_method :old_new, :new
+   def new(*args, &block)
+     obj = old_new(*args, &block)
+     self.included_modules.each do |mod|
+       mod.initialize(obj) if mod.respond_to?(:initialize)
+     end
+     obj
+   end
+ end
 
 module Initializable
   def self.included(base)
@@ -23,7 +23,7 @@ module Initializable
     def included(mod)
       if mod.class != Module #in case Initializerable is mixed-into a class
         puts "Adding #{self} to #{mod}'s included_modules" if $DEBUG
-        mod.included_modules << self
+         mod.initialized_modules << self
       end
     end
   end
