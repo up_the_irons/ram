126552e2206c65185454d85cda4c62b4dacaccf9
Mark Daggett <heavysixer@gmail.com>
2006-11-26 18:56:48 +0000

    Completed branch ticket_148 and merged with trunk
    Major Changes
    -----------------------------------------------
    Consolidated the migration files and removed the mysql specific code from the migrations.
    
    
    git-svn-id: svn+ssh://svn.locusfoc.us/home/svn/ram/trunk@1009 7bd17f4f-1217-0410-bbc5-bc4367116c30

--- db/migrate/014_add_category_id_to_asset.rb
+++
@@ -1,9 +0,0 @@
-class AddCategoryIdToAsset < ActiveRecord::Migration
-  def self.up
-    add_column :attachments, :category_id, :integer
-  end
-
-  def self.down
-    remove_column :attachments, :category_id
-  end
-end
--- db/migrate/016_add_protected_key_to_collections.rb
+++
@@ -1,9 +0,0 @@
-class AddProtectedKeyToCollections < ActiveRecord::Migration
-  def self.up
-    add_column :collections, :permanent, :boolean
-  end
-
-  def self.down
-    remove_column :collections, :permanent
-  end
-end
