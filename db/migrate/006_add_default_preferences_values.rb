b91ae939d6eaf517249d374570fade00402445db
Mark Daggett <heavysixer@gmail.com>
2006-12-28 09:51:49 +0000

    working on the migration bug.
    git-svn-id: svn+ssh://svn.locusfoc.us/home/svn/ram/trunk@1280 7bd17f4f-1217-0410-bbc5-bc4367116c30

--- db/migrate/006_add_default_preferences_values.rb
+++ db/migrate/006_add_default_preferences_values.rb
@@ -1,10 +1,10 @@
 class AddDefaultPreferencesValues < ActiveRecord::Migration
   def self.up
     Setting.transaction do
-      Setting.find(:all).each do |s| 
-        s.preferences = {:rmagick => true}
-        s.save!
-      end
+       Setting.find(:all).each do |s| 
+        s.preferences = {"rmagick" => true}
+        s.save || raise("Save failed")
+       end
     end
   end
 
