ed1d4ca52e365e12975a97c35ed74a53c4cf00e5
Garry Dolley <gdolley@arpnetworks.com>
2007-01-03 12:18:37 +0000

    This test script now saves the file it got with assets.get from API\\!
    git-svn-id: svn+ssh://svn.locusfoc.us/home/svn/ram/trunk@1326 7bd17f4f-1217-0410-bbc5-bc4367116c30

--- script/ram/ws_test2.rb
+++ script/ram/ws_test2.rb
@@ -6,4 +6,8 @@ server = XMLRPC::Client.new2("http://localhost:3030/services/api")
 
 # Using proxy
 ws = server.proxy('assets')
-puts ws.get('admin', 'admin', [1])
+file = ws.get('admin', 'admin', [1])[0]
+
+File.open(file['filename'], "w") do |f|
+  f.write(file['content'])
+end
